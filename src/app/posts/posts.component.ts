f4d639232b69bd99564a0230e25367f3414b4b05
Byamakesh <50134500+Byamakesh@users.noreply.github.com>
2020-08-21 16:55:24 +0530

    Implemented OnInit interface, performed npm fund and npm audit fix

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 
 @Component({
@@ -6,40 +6,44 @@ import { Http } from '@angular/http';
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export class PostsComponent {
+export class PostsComponent implements OnInit {
   posts: any;
   private url = 'http://jsonplaceholder.typicode.com/posts';
 
   constructor(private http: Http) {
-    http.get(this.url)
+
+  }
+
+  ngOnInit() {
+    this.http.get(this.url)
       .subscribe(response => {
         this.posts = response.json();
       });
   }
 
-  createPost(input: HTMLInputElement) {
-    let post = { title: input.value };
-    input.value = '';
+  // createPost(input: HTMLInputElement) {
+  //   let post = { title: input.value };
+  //   input.value = '';
 
-    this.http.post(this.url, JSON.stringify(post))
-      .subscribe(response => {
-        post['id'] = response.json().id;
-        this.posts.splice(0, 0, post);
-      });
-  }
+  //   this.http.post(this.url, JSON.stringify(post))
+  //     .subscribe(response => {
+  //       post['id'] = response.json().id;
+  //       this.posts.splice(0, 0, post);
+  //     });
+  // }
 
-  updatePost(post) {
-    this.http.put(this.url + '/' + post.id, JSON.stringify(post))
-      .subscribe(response => {
-        console.log(response.json());
-      });
-  }
+  // updatePost(post) {
+  //   this.http.put(this.url + '/' + post.id, JSON.stringify(post))
+  //     .subscribe(response => {
+  //       console.log(response.json());
+  //     });
+  // }
 
-  deletePost(post) {
-    this.http.delete(this.url + '/' + post.id)
-      .subscribe(response => {
-        let index = this.posts.indexOf(post);
-        this.posts.splice(index, 1);
-      })
-  }
+  //   deletePost(post) {
+  //     this.http.delete(this.url + '/' + post.id)
+  //       .subscribe(response => {
+  //         let index = this.posts.indexOf(post);
+  //         this.posts.splice(index, 1);
+  //       })
+  //   }
 }
